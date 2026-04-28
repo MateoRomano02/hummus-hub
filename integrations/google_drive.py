5b8bd4719cfbb2abc36fe3f2efb980409fbedf33
MateoRomano02 <romanomateo02@gmail.com>
2026-04-28 18:56:34 -0300

    feat: add google drive integration

---
+++ integrations/google_drive.py
@@ -0,0 +1,4 @@
+# Google Drive Service Account setup and fetch text logic
+def download_as_text(file_id: str) -> str:
+    # TODO: Implement Drive API fetching logic using service account
+    return ""
